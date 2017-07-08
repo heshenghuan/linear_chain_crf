5befcb8420a7f80758588d90e092d25876042141
heshenghuan <heshenghuan@sina.com>
2017-07-08 19:13:00 +0800

    environment parameters

---
+++ src/parameters.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 08 2017
+
+@author: Heshenghuan (heshenghuan@sina.com)
+http://github.com/heshenghuan
+
+Some env params may need to use.
+"""
+
+
+OOV = '_OOV_'
+START = '_S_'
+END = '_E_'
+GOLD_TAG = 'GoldNER'
+PRED_TAG = 'NER'
+task = 'ner'
+MAX_LEN = 175  # Max sequence length, because Weibo's input limitation
+
+LogP_ZERO = float('-inf')
+LogP_INF = float('inf')
+LogP_ONE = 0.0
+FloatX = 'float32'
+
+BASE_DIR = r'/Users/heshenghuan/Projects/linear_chainCRF/'
+MODEL_DIR = BASE_DIR + r'models/'
+DATA_DIR = BASE_DIR + r'data/'
+EMBEDDING_DIR = BASE_DIR + r'embeddings/'
+OUTPUT_DIR = BASE_DIR + r'export/'
+LOG_DIR = BASE_DIR + r'Summary/'
