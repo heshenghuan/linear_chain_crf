d0e65fa587ace334c401901aca9fcebc946d1f70
heshenghuan <heshenghuan@sina.com>
2017-08-24 20:54:56 +0800

    New feature template

--- src/features.py
+++ src/features.py
@@ -7,53 +7,86 @@ Created on Sat Jul 08 2017
 http://github.com/heshenghuan
 """
 
-
-import numpy as np
+import os
+import codecs as cs
 from parameters import START, END
 
-# Separator of field values.
-separator = "\t"
+
+class Template(object):
+    """
+    Context-based feature template.
+    """
+    def __init__(self, fn=None, sep=', '):
+        """
+        Returns a templates instance from file.
+        """
+        self._fields = None
+        self._template = None
+        self._valid = False
+
+        if fn is None:
+            raise ValueError('Initializer got a \'None\' instead a file path.')
+        else:
+            self.build(fn, sep)
+
+    def build(self, fn, sep):
+        self._fields = []
+        self._template = []
+        with cs.open(fn, 'r', 'utf8') as src:
+            lines = src.read().strip().split('\n')
+            # read fields from file
+            self._fields = lines[1].strip().split()
+            # read templates
+            for i in range(3, len(lines)):
+                temp = []
+                line = lines[i]
+                for t in line.strip().split(sep):
+                    field, offset = t.strip().split(':')
+                    if field not in self._fields:
+                        p = os.path.abspath(fn)
+                        e = "Unknow field %s, File \"%s\", line %d" % (field, p, i+1)
+                        raise ValueError(e)
+                    temp.append((field, int(offset)))
+                self._template.append(temp)
+            self._valid = True
+
+    @property
+    def template(self):
+        return self._template
+
+    @template.setter
+    def template(self, *argv, **argw):
+        pass
+
+    @property
+    def fields(self):
+        return self._fields
+
+    @fields.setter
+    def fields(self, *argv, **argw):
+        pass
+
+    @property
+    def valid(self):
+        return self._valid
+
 
 # Field names of the input data.
-fields = ['w', 'y']
-pmi_fields = ['w', 'p', 'y']
+# fields = ['w', 'y']
 
 # Attribute templates.
-templates = (
-    (('r', -2), ),
-    (('r', -1), ),
-    (('r',  0), ),
-    (('r',  1), ),
-    (('r',  2), ),
-    (('r', -2), ('r', -1)),
-    (('r', -1), ('r',  0)),
-    (('r',  0), ('r',  1)),
-    (('r',  1), ('r',  2)),
-    (('r', -1), ('r',  1)),
-    )
-
-pmi_templates = (
-    (('r', -2), ),
-    (('r', -1), ),
-    (('r',  0), ),
-    (('r',  1), ),
-    (('r',  2), ),
-    (('r', -2), ('r', -1)),
-    (('r', -1), ('r',  0)),
-    (('r',  0), ('r',  1)),
-    (('r',  1), ('r',  2)),
-    (('r', -1), ('r',  1)),
-    (('p', -2), ),
-    (('p', -1), ),
-    (('p',  0), ),
-    (('p',  1), ),
-    (('p',  2), ),
-    (('p', -2), ('p', -1)),
-    (('p', -1), ('p',  0)),
-    (('p',  0), ('p',  1)),
-    (('p',  1), ('p',  2)),
-    (('p', -1), ('p',  1)),
-    )
+# templates = (
+#     (('w', -2), ),
+#     (('w', -1), ),
+#     (('w',  0), ),
+#     (('w',  1), ),
+#     (('w',  2), ),
+#     (('w', -2), ('w', -1)),
+#     (('w', -1), ('w',  0)),
+#     (('w',  0), ('w',  1)),
+#     (('w',  1), ('w',  2)),
+#     (('w', -1), ('w',  1)),
+#     )
 
 
 def readiter(data, names):
@@ -84,7 +117,7 @@ def readiter(data, names):
     return X
 
 
-def apply_templates(X, templates=templates):
+def apply_templates(X, templates=None):
     """
     Generate features for an item sequence by applying feature templates.
     A feature template consists of a tuple of (name, offset) pairs,
@@ -94,10 +127,12 @@ def apply_templates(X, templates=templates):
 
     @type   X:  A list of dict{'w':w, 'r':r, 'y':y, F:[]}
     @param  X:  The item sequence.
-    @type   template:   tuple of (str, int)
+    @type   template:   list
     @param  template:   The feature template.
     """
     # print 'in apply templates! input:', X
+    if templates is None:
+        raise TypeError("templates should be iterable type not NoneType")
     length = len(X)
     for template in templates:
         name = '|'.join(['%s[%d]' % (f, o) for f, o in template])
@@ -152,6 +187,8 @@ def output_features(fo, X, field=''):
     fo.write('\n')
 
 
-def feature_extractor(X, templates=templates):
+def feature_extractor(X, templates=None):
     # Apply attribute templates to obtain features (in fact, attributes)
+    if templates is None:
+        raise TypeError("templates should be iterable type not NoneType")
     return apply_templates(X, templates)
