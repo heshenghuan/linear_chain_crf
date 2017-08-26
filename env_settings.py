31bcf11715dfb3e53ba8ce8c791ee4ad0bfcd3ba
heshenghuan <heshenghuan@sina.com>
2017-08-26 22:38:56 +0800

    enviroment settings file

---
+++ env_settings.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 26 2017
+
+@author: Heshenghuan (heshenghuan@sina.com)
+http://github.com/heshenghuan
+"""
+
+import os
+
+# BASE_DIR = r'/Users/heshenghuan/Projects/linear_chainCRF/'
+BASE_DIR = os.getcwd() + '/'
+MODEL_DIR = BASE_DIR + r'models/'
+DATA_DIR = BASE_DIR + r'data/'
+EMB_DIR = BASE_DIR + r'embeddings/'
+OUTPUT_DIR = BASE_DIR + r'export/'
+LOG_DIR = BASE_DIR + r'Summary/'
