2bc5c5c9cb7712a11f5639b113fdfdbbaf41fb3a
heshenghuan <heshenghuan@sina.com>
2017-09-12 12:02:36 +0800

    env_settings

--- env_settings.py
+++ env_settings.py
@@ -16,3 +16,12 @@ DATA_DIR = BASE_DIR + r'data/'
 EMB_DIR = BASE_DIR + r'embeddings/'
 OUTPUT_DIR = BASE_DIR + r'export/'
 LOG_DIR = BASE_DIR + r'Summary/'
+
+if __name__ == "__main__":
+    # These three dirs must be exist, because they are default output dirs
+    dirs = [MODEL_DIR, OUTPUT_DIR, LOG_DIR]
+    for d in dirs:
+        if not os.path.exists(d):
+            print d, "not exists."
+            print "Automatically created it."
+            os.makedirs(d)
